cdb843f9d11f50408ee84463205cb6165c5dc30a
Rob Whitaker <rob.whitaker.dev@gmail.com>
2025-02-16 16:19:00 -0500

    Changed the embeds parameter to embed when updating the message with an embed.

--- commands.rb
+++ commands.rb
@@ -30,7 +30,7 @@ def register_commands(bot)
                          FOOTER_TEXT, TROPHY_IMAGE_URL)
 
     begin
-      message.edit(embeds: [embed])
+      message.edit(content: "", embed: embed)
     rescue Discordrb::Errors::NoPermission
       event.respond "I don't have permission to edit messages in the 'week-advances' channel. Please check my permissions."
     end
